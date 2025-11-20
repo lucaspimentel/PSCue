cb4cee967aa94632e021205016583cf44ebd7daf
Lucas Pimentel <lucaspimentel@gmail.com>
2025-11-20 16:24:31 -0500

    delete cmd scripts

--- scripts/install-local.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Install PSCue module by building from source
-REM This script runs the PowerShell installation script using pwsh.
-
-pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-local.ps1" %*
--- scripts/install-remote.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Install PSCue module by downloading pre-built binaries from GitHub
-REM This script runs the PowerShell installation script using pwsh.
-
-pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-remote.ps1" %*
